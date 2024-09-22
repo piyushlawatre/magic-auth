2228e792201fa0a5783eadee55f7afbdc0010266
piyush-lawatre-coditas <piyush.lawatre@coditas.com>
2024-09-22 15:55:23 +0530

    adding sign up user API

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,24 @@
+import Email from "../utility/email.js";
+import User from "../models/user.js";
+
 export const signUp = async (req, res, next) => {
   try {
-    res.json({
-      statusCode: 200,
-      message: "signup ",
+    const newUser = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      confirmPassword: req.body.confirmPassword,
+    });
+
+    const email = new Email(
+      newUser,
+      `${req.protocol}://${req.get("host")}/api/v1/users/login`
+    );
+    await email.sendWelcome();
+
+    res.status(201).json({
+      status: "success",
+      message: "User created successfully",
     });
   } catch (error) {
     next(error);
