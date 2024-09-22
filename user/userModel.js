c718e7efcf0f52a67075f9176fe5ae0ebf0cba9e
piyush-lawatre-coditas <piyush.lawatre@coditas.com>
2024-09-22 15:30:32 +0530

    making user schema and required method for pass hash

---
+++ user/userModel.js
@@ -0,0 +1,52 @@
+import { Schema, model } from "mongoose";
+import { isEmail } from "validator";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "A user must have a name"],
+  },
+  email: {
+    type: String,
+    required: [true, "A user must have an email"],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, "Provide a valid email"],
+  },
+  password: {
+    type: String,
+    select: false,
+    required: [true, "Please provide a password"],
+    minLength: [8, "Password should have at least 8 characters"],
+  },
+  confirmPassword: {
+    type: String,
+    required: [true, "Please provide a confirm password"],
+    validate: {
+      validator: function (confirmPassword) {
+        return confirmPassword === this.password;
+      },
+      message: "Passwords do not match",
+    },
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  this.confirmPassword = undefined;
+  next();
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = model("User", userSchema);
+export default User;
